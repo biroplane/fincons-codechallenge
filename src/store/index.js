10942dda19413593fc34593e54b9fe0c08dbff21
Gaetano Vallarelli <biroplane@users.noreply.github.com>
2022-06-20 17:31:56 +0200

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,33 @@
+/* eslint-disable prettier/prettier */
+import Axios from "axios";
 import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    people: []
+  },
+  getters: {
+    people: (state) => state.people || []
+  },
+
+  actions: {
+    async loadPeople({ commit }) {
+      let _people = await Axios({
+        method: "GET",
+        url: "https://services.odata.org/TripPinRESTierService/(S(hespbvdrrmhquk5vqlzcpbro))/People"
+      });
+
+      console.log("People loaded ", _people);
+      commit("SET_PEOPLE", _people.data.value);
+      // commit("SET_PEOPlE", _people.data);
+    }
+  },
+  mutations: {
+    SET_PEOPLE: (state, people) => {
+      state.people = [...people];
+    }
+  }
 });
